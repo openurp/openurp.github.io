c0ad80d2e58225179c62cac4d0578a2b2dd7fc23
duantihua <duantihua@users.noreply.github.com>
2026-01-07 16:57:31 +0800

    deploy: aa7a9c61f29ff17b4ba5612567aa654adacd73aa

--- db/postgresql/init/seed.sql
+++ db/postgresql/init/seed.sql
@@ -171,7 +171,91 @@ INSERT INTO code.student_statuses (id, code, name, en_name, begin_on, end_on, up
 INSERT INTO code.student_statuses (id, code, name, en_name, begin_on, end_on, updated_at, remark) VALUES (4, '04', '延期', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
 INSERT INTO code.student_statuses (id, code, name, en_name, begin_on, end_on, updated_at, remark) VALUES (5, '05', '未入学', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
 
-INSERT INTO edu.room_occupy_apps (id, name, activity_url) VALUES (1, 'course', '
+INSERT INTO edu.room_occupy_apps (id, name, activity_url) VALUES (1, 'course', '--');
+INSERT INTO edu.room_occupy_apps (id, name, activity_url) VALUES (2, 'exam', '--');
+INSERT INTO edu.room_occupy_apps (id, name, activity_url) VALUES (3, 'apply', '--');
+
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (1, '01', '汉族', 'HA',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (02, '02', '蒙古族','MG', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (03, '03', '回族', 'HU',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (04, '04', '藏族','ZA', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (05, '05', '维吾尔族','UG', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (06, '06', '苗族','MH', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (07, '07', '彝族','YI', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (08, '08', '壮族', 'ZH',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (09, '09', '布依族', 'BY',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (10, '10', '朝鲜族', 'CS',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (11, '11', '满族', 'MA',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (12, '12', '侗族', 'DO',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (14, '14', '白族', 'MA',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (15, '15', '土家族','TJ', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (17, '17', '哈萨克族','KZ', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (22, '22', '畲族','SH', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (23, '23', '高山族','GS', NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (33, '33', '羌族', 'QI',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (37, '37', '仡佬族', 'GL',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (38, '38', '锡伯族', 'XB',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+INSERT INTO code.nations (id, code, name,alpha_code, en_name, begin_on, end_on, updated_at, remark) VALUES (97, '97', '其他', 'QT',NULL, '2015-06-23', NULL, '2015-06-23 00:00:00', NULL);
+
+insert into base.c_education_types(id,code,name,begin_on,updated_at) values(1,'1','普通本科',current_date -10,now());
+insert into base.c_std_types(id,code,name,begin_on,updated_at) values(1,'1','普通',current_date -10,now());
+
+INSERT INTO code.std_alter_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (1, '2017-04-14', '1', NULL, NULL, '休学', '2017-04-14 14:40:00.813518');
+INSERT INTO code.std_alter_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (2, '2017-04-14', '2', NULL, NULL, '退学', '2017-04-14 14:40:24.358813');
+INSERT INTO code.std_alter_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (3, '2017-04-14', '3', NULL, NULL, '复学', '2017-04-14 14:41:06.502864');
+INSERT INTO code.std_alter_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (4, '2017-04-14', '4', NULL, NULL, '放弃学籍', '2017-04-14 14:41:36.211759');
+INSERT INTO code.std_alter_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (5, '2017-04-14', '5', NULL, NULL, '转专业', '2017-04-14 14:42:10.183278');
+INSERT INTO code.std_alter_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (6, '2017-04-26', '6', NULL, NULL, '恢复学籍', '2017-04-26 22:13:33.004115');
+INSERT INTO code.std_alter_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (7, '2017-04-26', '7', NULL, NULL, '延期毕业', '2017-04-26 22:13:33.004115');
+INSERT INTO code.std_alter_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (11, '2019-03-31', '11', NULL, NULL, '保留学籍', '2019-04-02 16:12:20.816552');
+
+INSERT INTO code.std_alter_reasons(id, begin_on, code, en_name, end_on, name, updated_at) VALUES (11, '2019-03-31', '11', NULL, NULL, '个人原因', '2019-04-02 16:12:20.816552');
+
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(1,'01','哲学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(2,'02','经济学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(3,'03','法学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(4,'04','教育学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(5,'05','文学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(6,'06','历史学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(7,'07','理学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(8,'08','工学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(9,'09','农学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(10,'10','医学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(11,'11','军事学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(12,'12','管理学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(13,'13','艺术学',current_date -10*365,now());
+insert into code.discipline_categories(id,code,name,begin_on,updated_at) values(14,'14','交叉学科',current_date -10*365,now());
+--学校性质类别
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(1,'01','综合大学',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(2,'02','理工院校',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(3,'03','农林院校',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(4,'04','林业院校',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(5,'05','医药院校',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(6,'06','师范院校',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(7,'07','语文院校',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(8,'08','财经院校',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(9,'09','政法院校',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(10,'10','体育院校',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(11,'11','艺术院校',current_date -10,now());
+insert into code.institution_categories(id,code,name,begin_on,updated_at) values(12,'12','民族院校',current_date -10,now());
+
+--建筑物用途
+INSERT INTO code.building_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (11, '2019-03-31', '11', NULL, NULL, '教学楼', '2019-04-02 16:12:20.816552');
+INSERT INTO code.building_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (12, '2019-03-31', '12', NULL, NULL, '综合楼', '2019-04-02 16:12:20.816552');
+INSERT INTO code.building_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (13, '2019-03-31', '13', NULL, NULL, '实验楼', '2019-04-02 16:12:20.816552');
+INSERT INTO code.building_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (14, '2019-03-31', '14', NULL, NULL, '图书馆(室)', '2019-04-02 16:12:20.816552');
+INSERT INTO code.building_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (15, '2019-03-31', '15', NULL, NULL, '体育活动室', '2019-04-02 16:12:20.816552');
+INSERT INTO code.building_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (21, '2019-03-31', '21', NULL, NULL, '宿舍', '2019-04-02 16:12:20.816552');
+INSERT INTO code.building_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (31, '2019-03-31', '31', NULL, NULL, '办公楼', '2019-04-02 16:12:20.816552');
+INSERT INTO code.building_types (id, begin_on, code, en_name, end_on, name, updated_at) VALUES (41, '2019-03-31', '41', NULL, NULL, '其他用房', '2019-04-02 16:12:20.816552');
+
+--部门类别
+insert into code.department_categories(id,code,name,indexno,begin_on,updated_at) values(10,'10','党政机构','10',current_date-1,now());
+insert into code.department_categories(id,code,name,indexno,begin_on,updated_at) values(20,'20','教学科研单位','20',current_date-1,now());
+insert into code.department_categories(id,code,name,indexno,begin_on,updated_at) values(21,'21','科研机构','21',current_date-1,now());
+insert into code.department_categories(id,code,name,indexno,begin_on,updated_at) values(30,'30','直属单位','30',current_date-1,now());
+
+-----------如下代码需要插入后修改---------
 insert into code.institutions(id,code,name,begin_on,updated_at)
 values(10000,'10000','URP学院',current_date -10,now());
 
